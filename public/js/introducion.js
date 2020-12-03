4463fcda1e4765dc3700e0a85fe97c8015d988cb
wasantac <34143200+wasantac@users.noreply.github.com>
2020-12-02 19:16:52 -0500

    Navegacion Guiada
    Completa y Funcional

---
+++ public/js/introducion.js
@@ -0,0 +1,26 @@
+const intro = introJs();
+intro.setOptions({
+    steps:[
+        {
+            intro: "Bienvenido a nuestro sitio web! Aqui un pequeño tour."
+        },
+        {
+            element : "#publicidad",
+            intro: "Aqui puede encontrar la publicidad del sitio."
+        },
+        { 
+            element: "nav",
+            intro: "Aqui la barra de navegacion con botenes para que navegues por el sitio."
+
+        },
+        {
+            element: "#Electrodomesticos",
+            intro: "Puedes encontrar rapidamente productos que estan distribuidos por categorias."
+        },
+        {
+            element: "#pie1",
+            intro: "Finalmente puedes ver nuestras redes e informacion para que nos contactes :)"
+        }
+    ]
+})
+intro.start();
