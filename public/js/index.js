eecc57d4c9cffd394027bca098b1ce638be28ef0
Ricavill <56647079+Ricavill@users.noreply.github.com>
2020-12-03 18:47:59 -0500

    CargarElectrodomesticos

--- public/js/index.js
+++ public/js/index.js
@@ -31,8 +31,55 @@ $(function() {
 
      })
  }
+ cargarElectrodomesticos=()=>{
+   fetch("/assets/json/productos.json")
+   .then((response)=>response.json())
+   .then((str)=>{
+     productos=str.productos;
+
+     let k=0;
+     let i=0;
+
+     while(k<5){
+       if(productos[i].tipo=="Electrodomesticos"){
+         electros=document.getElementsByClassName("Electro")
+         for(electro of electros){
+           let electomulti;
+           if(electro.id=="Electrodomesticos"){
+             electromulti=electro.getElementsByClassName("multiple-items")[0];
+             console.log("Aqui multiple")
+           }
+           else{
+             electromulti=electro.getElementsByClassName("single-items")[0];
+             console.log("Aqui single")
+           }
+           divinterno=electromulti.getElementsByTagName("div")[0];
+
+
+           let electrocard=divinterno.getElementsByClassName("card")[k];
+           let cardimg=electrocard.getElementsByTagName("img")[0];
+           cardimg.src=productos[i].imageurl;
+
+         }
+         k++;
+       }
+       if(i>=productos.length){
+         i=0;
+       }
+       else{
+         i++;
+       }
+
+     }
+   })
+   .catch((error)=>{
+     console.log("Error: "+error)
+   })
+ }
+
 
  document.addEventListener('DOMContentLoaded', function() {
      cargarNoticias();
+     cargarElectrodomesticos();
 
  })
