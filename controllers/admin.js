f4f1bb44720d9fe7031fc3c5789684e9cb6adcd3
Evelyn Mejia <nohelia.mejia99@gmail.com>
2020-12-04 07:36:41 -0500

    perfil y stepper

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,6 +12,9 @@ exports.analytics = function(req, res) {
     res.render(path.resolve('views/admin/analytics.html'))
 }
 
+exports.profile = function(req, res) {
+    res.render(path.resolve('views/admin/profile.html'))
+}
 
 exports.components = function(req, res) {
     res.render(path.resolve('views/admin/components.html'))
